898b219dd149d94fdffc2f4fce773baab0376655
charcoJP <charco2356@gmail.com>
2018-10-04 22:40:41 +0900

    add warning code

--- app/src/main/java/jp/co/charco/dangerexample/MainActivity.kt
+++ app/src/main/java/jp/co/charco/dangerexample/MainActivity.kt
@@ -2,8 +2,11 @@ package jp.co.charco.dangerexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.IconCompatParcelizer
 
 class MainActivity : AppCompatActivity() {
+
+    private val hoge = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
